d04f984a7450159d3736f0764d3ce852923637d2
feiyu <lvp123321123321@gmail.com>
2025-09-04 08:40:48 -0400

    improve send() behaviour in linux

--- src/extensions/overload.h
+++ src/extensions/overload.h
@@ -531,7 +531,7 @@ struct Overload {
 #ifdef _MSC_VER
 #define MSG_NOSIGNAL 0
 #endif
-            int sentBytes = send(tcpData->socket, (const char*)fragment.FragmentBuffer + totalSentBytes, fragment.FragmentLength - totalSentBytes, MSG_NOSIGNAL);
+            int sentBytes = send(tcpData->socket, (const char*)fragment.FragmentBuffer + totalSentBytes, fragment.FragmentLength - totalSentBytes, MSG_NOSIGNAL | MSG_DONTWAIT);
             if (sentBytes == 0) {
                 // connection closed
                 Token->CompletionToken.Status = -1;
@@ -541,28 +541,27 @@ struct Overload {
 #ifdef _MSC_VER
                 int err = WSAGetLastError();
                 if (err == WSAEWOULDBLOCK) {
-                    logToConsole(L"Need to wait to send bytes");
-                    // wait until socket is writable
-                    fd_set wfds;
-                    FD_ZERO(&wfds);
-                    FD_SET(tcpData->socket, &wfds);
-
-                    int timeout_ms = 500; // no timeout
-
-                    TIMEVAL tv;
-                    TIMEVAL* ptv = nullptr;
-                    if (timeout_ms >= 0) {
-                        tv.tv_sec = timeout_ms / 1000;
-                        tv.tv_usec = (timeout_ms % 1000) * 1000;
-                        ptv = &tv;
-                    }
-
-                    int ret = select(0, nullptr, &wfds, nullptr, ptv);
-                    if (ret <= 0) {
-                        // timeout or error
-                        Token->CompletionToken.Status = -1;
-                        return EFI_ABORTED;
-                    }
+                    // // wait until socket is writable
+                    // fd_set wfds;
+                    // FD_ZERO(&wfds);
+                    // FD_SET(tcpData->socket, &wfds);
+                    //
+                    // int timeout_ms = 500; // no timeout
+                    //
+                    // TIMEVAL tv;
+                    // TIMEVAL* ptv = nullptr;
+                    // if (timeout_ms >= 0) {
+                    //     tv.tv_sec = timeout_ms / 1000;
+                    //     tv.tv_usec = (timeout_ms % 1000) * 1000;
+                    //     ptv = &tv;
+                    // }
+                    //
+                    // int ret = select(0, nullptr, &wfds, nullptr, ptv);
+                    // if (ret <= 0) {
+                    //     // timeout or error
+                    //     Token->CompletionToken.Status = -1;
+                    //     return EFI_ABORTED;
+                    // }
                     continue; // retry send
 				}
 				else {
@@ -572,15 +571,15 @@ struct Overload {
 #else
                 if (errno == EAGAIN || errno == EWOULDBLOCK)
                 {
-                    pollfd pfd;
-                    pfd.fd = tcpData->socket;
-                    pfd.events = POLLOUT;
-                    int ret = poll(&pfd, 1, 500);
-                    if (ret <= 0) {
-                        // timeout or error
-                        Token->CompletionToken.Status = -1;
-                        return EFI_ABORTED;
-                    }
+                    // pollfd pfd;
+                    // pfd.fd = tcpData->socket;
+                    // pfd.events = POLLOUT;
+                    // int ret = poll(&pfd, 1, -1);
+                    // if (ret <= 0) {
+                    //     // timeout or error
+                    //     Token->CompletionToken.Status = -1;
+                    //     return EFI_ABORTED;
+                    // }
                     continue; // retry
                 } else
                 {
