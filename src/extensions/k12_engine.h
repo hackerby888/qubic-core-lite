5f4bedad2d9bc9c174ef03d477646edd13966e86
feiyu <lvp123321123321@gmail.com>
2026-01-23 21:36:47 +0700

    improve k12 engine

--- src/extensions/k12_engine.h
+++ src/extensions/k12_engine.h
@@ -2,6 +2,7 @@
 #include <K12/kangaroo_twelve_xkcp.h>
 #include <cstring>
 #include <unordered_map>
+#include <vector>
 
 class K12Engine
 {
@@ -10,125 +11,130 @@ class K12Engine
         unsigned char intermediate[maxCapacityInBytes];
     };
 
-    std::unordered_map<unsigned int, Intermediate> intermediateMap;
-    std::unordered_map<unsigned int, bool> isChunkChangedMap;
+    // map from chunk index to intermediate state
+    std::vector<Intermediate> intermediateMap;
+    // map from chunk index to isChanged flag
+    std::vector<bool> isChunkChangedMap;
     unsigned int maxChunks;
     unsigned char *_state;
     size_t _stateSize;
 
-private:
-    int _KangarooTwelve_Update(XKCP::KangarooTwelve_Instance* ktInstance, const unsigned char* input, size_t inputByteLen)
-{
-    if (ktInstance->phase != XKCP::ABSORBING)
-        return 1;
-
-    if (ktInstance->blockNumber == 0)
+    int _KangarooTwelve_Update(XKCP::KangarooTwelve_Instance *ktInstance, const unsigned char *input, size_t inputByteLen)
     {
-        /* First block, absorb in final node */
-        unsigned int len = (inputByteLen < (K12_chunkSize - ktInstance->queueAbsorbedLen)) ? (unsigned int)inputByteLen : (K12_chunkSize - ktInstance->queueAbsorbedLen);
-        XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, input, len);
-        input += len;
-        inputByteLen -= len;
-        ktInstance->queueAbsorbedLen += len;
-        if ((ktInstance->queueAbsorbedLen == K12_chunkSize) && (inputByteLen != 0))
-        {
-            /* First block complete and more input data available, finalize it */
-            const unsigned char padding = 0x03; /* '110^6': message hop, simple padding */
-            ktInstance->queueAbsorbedLen = 0;
-            ktInstance->blockNumber = 1;
-            XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, &padding, 1);
-            ktInstance->finalNode.byteIOIndex = (ktInstance->finalNode.byteIOIndex + 7) & ~7; /* Zero padding up to 64 bits */
-        }
-    }
-    else if (ktInstance->queueAbsorbedLen != 0)
-    {
-        /* There is data in the queue, absorb further in queue until block complete */
-        unsigned int len = (inputByteLen < (K12_chunkSize - ktInstance->queueAbsorbedLen)) ? (unsigned int)inputByteLen : (K12_chunkSize - ktInstance->queueAbsorbedLen);
-        XKCP::TurboSHAKE_Absorb(&ktInstance->queueNode, input, len);
-        input += len;
-        inputByteLen -= len;
-        ktInstance->queueAbsorbedLen += len;
-        if (ktInstance->queueAbsorbedLen == K12_chunkSize)
-        {
-            int capacityInBytes = 2 * (ktInstance->securityLevel) / 8;
-            unsigned char intermediate[maxCapacityInBytes];
-            // assert(capacityInBytes <= maxCapacityInBytes);
-            ktInstance->queueAbsorbedLen = 0;
-            ++ktInstance->blockNumber;
-            XKCP::TurboSHAKE_AbsorbDomainSeparationByte(&ktInstance->queueNode, K12_suffixLeaf);
-            XKCP::TurboSHAKE_Squeeze(&ktInstance->queueNode, intermediate, capacityInBytes);
-            XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, intermediate, capacityInBytes);
-        }
-    }
+        if (ktInstance->phase != XKCP::ABSORBING)
+            return 1;
 
-
-    while (inputByteLen > 0)
-    {
-        int capacityInBytes = 2 * (ktInstance->securityLevel) / 8;
-        unsigned int len = (inputByteLen < K12_chunkSize) ? (unsigned int)inputByteLen : K12_chunkSize;
-        unsigned int chunkIndex = ktInstance->blockNumber;
-
-        if (!isChunkChangedMap[chunkIndex])
+        if (ktInstance->blockNumber == 0)
         {
-            if (len == K12_chunkSize)
+            /* First block, absorb in final node */
+            unsigned int len = (inputByteLen < (K12_chunkSize - ktInstance->queueAbsorbedLen)) ? (unsigned int)inputByteLen : (K12_chunkSize - ktInstance->queueAbsorbedLen);
+            XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, input, len);
+            input += len;
+            inputByteLen -= len;
+            ktInstance->queueAbsorbedLen += len;
+            if ((ktInstance->queueAbsorbedLen == K12_chunkSize) && (inputByteLen != 0))
             {
-                unsigned char *intermediate = intermediateMap[chunkIndex].intermediate;
-                XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, intermediate, capacityInBytes);
-                input += len;
-                inputByteLen -= len;
+                /* First block complete and more input data available, finalize it */
+                const unsigned char padding = 0x03; /* '110^6': message hop, simple padding */
+                ktInstance->queueAbsorbedLen = 0;
+                ktInstance->blockNumber = 1;
+                XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, &padding, 1);
+                ktInstance->finalNode.byteIOIndex = (ktInstance->finalNode.byteIOIndex + 7) & ~7; /* Zero padding up to 64 bits */
+            }
+        }
+        else if (ktInstance->queueAbsorbedLen != 0)
+        {
+            /* There is data in the queue, absorb further in queue until block complete */
+            unsigned int len = (inputByteLen < (K12_chunkSize - ktInstance->queueAbsorbedLen)) ? (unsigned int)inputByteLen : (K12_chunkSize - ktInstance->queueAbsorbedLen);
+            XKCP::TurboSHAKE_Absorb(&ktInstance->queueNode, input, len);
+            input += len;
+            inputByteLen -= len;
+            ktInstance->queueAbsorbedLen += len;
+            if (ktInstance->queueAbsorbedLen == K12_chunkSize)
+            {
+                int capacityInBytes = 2 * (ktInstance->securityLevel) / 8;
+                unsigned char intermediate[maxCapacityInBytes];
+                // assert(capacityInBytes <= maxCapacityInBytes);
+                ktInstance->queueAbsorbedLen = 0;
                 ++ktInstance->blockNumber;
-                continue;
+                XKCP::TurboSHAKE_AbsorbDomainSeparationByte(&ktInstance->queueNode, K12_suffixLeaf);
+                XKCP::TurboSHAKE_Squeeze(&ktInstance->queueNode, intermediate, capacityInBytes);
+                XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, intermediate, capacityInBytes);
             }
         }
 
-        XKCP::TurboSHAKE_Initialize(&ktInstance->queueNode, ktInstance->securityLevel);
-        XKCP::TurboSHAKE_Absorb(&ktInstance->queueNode, input, len);
-        input += len;
-        inputByteLen -= len;
-        if (len == K12_chunkSize)
+        while (inputByteLen > 0)
         {
-            unsigned char intermediate[maxCapacityInBytes];
-            // assert(capacityInBytes <= maxCapacityInBytes);
-            ++ktInstance->blockNumber;
-            XKCP::TurboSHAKE_AbsorbDomainSeparationByte(&ktInstance->queueNode, K12_suffixLeaf);
-            XKCP::TurboSHAKE_Squeeze(&ktInstance->queueNode, intermediate, capacityInBytes);
-            XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, intermediate, capacityInBytes);
+            int capacityInBytes = 2 * (ktInstance->securityLevel) / 8;
+            unsigned int len = (inputByteLen < K12_chunkSize) ? (unsigned int)inputByteLen : K12_chunkSize;
+            unsigned int chunkIndex = ktInstance->blockNumber;
 
-            // cache the intermediate state
-            Intermediate &inter = intermediateMap[chunkIndex];
-            std::memcpy(inter.intermediate, intermediate, capacityInBytes);
-            isChunkChangedMap[chunkIndex] = false;
+            if (!isChunkChangedMap[chunkIndex])
+            {
+                if (len == K12_chunkSize)
+                {
+                    unsigned char *intermediate = intermediateMap[chunkIndex].intermediate;
+                    XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, intermediate, capacityInBytes);
+                    input += len;
+                    inputByteLen -= len;
+                    ++ktInstance->blockNumber;
+                    continue;
+                }
+            }
+
+            XKCP::TurboSHAKE_Initialize(&ktInstance->queueNode, ktInstance->securityLevel);
+            XKCP::TurboSHAKE_Absorb(&ktInstance->queueNode, input, len);
+            input += len;
+            inputByteLen -= len;
+            if (len == K12_chunkSize)
+            {
+                unsigned char intermediate[maxCapacityInBytes];
+                // assert(capacityInBytes <= maxCapacityInBytes);
+                ++ktInstance->blockNumber;
+                XKCP::TurboSHAKE_AbsorbDomainSeparationByte(&ktInstance->queueNode, K12_suffixLeaf);
+                XKCP::TurboSHAKE_Squeeze(&ktInstance->queueNode, intermediate, capacityInBytes);
+                XKCP::TurboSHAKE_Absorb(&ktInstance->finalNode, intermediate, capacityInBytes);
+
+                // cache the intermediate state
+                Intermediate &inter = intermediateMap[chunkIndex];
+                std::memcpy(inter.intermediate, intermediate, capacityInBytes);
+                isChunkChangedMap[chunkIndex] = false;
+            }
+            else
+            {
+                ktInstance->queueAbsorbedLen = len;
+            }
         }
-        else
-        {
-            ktInstance->queueAbsorbedLen = len;
-        }
+
+        return 0;
     }
 
-    return 0;
-}
-
 public:
-
     K12Engine(unsigned char *state, size_t stateSize)
     {
         _state = state;
         _stateSize = stateSize;
         maxChunks = (stateSize + K12_chunkSize - 1) / K12_chunkSize;
 
+        isChunkChangedMap.reserve(maxChunks);
+        intermediateMap.resize(maxChunks);
         for (unsigned int i = 0; i < maxChunks; i++)
         {
             isChunkChangedMap[i] = true;
         }
+        for (unsigned int i = 0; i < maxChunks; i++)
+        {
+            std::memset(intermediateMap[i].intermediate, 0, maxCapacityInBytes);
+        }
     }
 
-    int getHash128( unsigned char* output, size_t outputByteLen)
+    int getHash(unsigned char* output, size_t outputByteLen, int securityLevel = 128)
     {
         XKCP::KangarooTwelve_Instance ktInstance;
 
         if (outputByteLen == 0)
             return 1;
-        XKCP::KangarooTwelve_Initialize(&ktInstance, 128, outputByteLen);
+        XKCP::KangarooTwelve_Initialize(&ktInstance, securityLevel, outputByteLen);
         if (_KangarooTwelve_Update(&ktInstance, _state, _stateSize) != 0)
             return 1;
         return XKCP::KangarooTwelve_Final(&ktInstance, output, nullptr, 0);
@@ -141,4 +147,5 @@ public:
             isChunkChangedMap[chunkIndex] = true;
         }
     }
+    unsigned int getMaxChunks() const { return maxChunks; }
 };
--- test/k12_engine.cpp
+++ test/k12_engine.cpp
@@ -1,23 +1,35 @@
 #define NO_UEFI
 #include "platform/m256.h"
-#include <iostream>
 #include <chrono>
 #include <extensions/k12_engine.h>
 #include <gtest/gtest.h>
+#include <immintrin.h>
+#include <iostream>
 
-TEST(K12EngineTest, PlaceholderTest) {
-    size_t ONE_GB = 1024 * 1024 * 1024;
+TEST(K12EngineTest, GeneralTest) {
+    size_t ONE_GB = 1024 * 1024 * 1024 - 64;
     unsigned char *state = new unsigned char[ONE_GB];
     K12Engine k12Engine(state, ONE_GB);
     memset(state, 0, ONE_GB);
 
+    unsigned long long ranVal;
+    _rdrand64_step(&ranVal);
+    for (size_t i = 0; i < ONE_GB; i += 8) {
+        if (i + 8 >= ONE_GB)
+        {
+            break;
+        }
+        std::memcpy(state + i, &ranVal, sizeof(unsigned long long));
+        ranVal++;
+    }
+
     m256i hash1;
     m256i hash2;
     auto startTime = std::chrono::high_resolution_clock::now();
-    k12Engine.getHash128(hash1.m256i_u8, 32);
-    k12Engine.getHash128(hash2.m256i_u8, 32);
+    k12Engine.getHash(hash1.m256i_u8, 32);
+    k12Engine.getHash(hash2.m256i_u8, 32);
     auto durationMilliSec = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startTime);
-    std::cout << "K12Engine getHash128 called twice on 1 GB input took " << durationMilliSec.count() << " ms" << std::endl;
+    std::cout << "K12Engine getHash called twice on 1 GB input took " << durationMilliSec.count() << " ms" << std::endl;
 
     {
         startTime = std::chrono::high_resolution_clock::now();
@@ -31,14 +43,18 @@ TEST(K12EngineTest, PlaceholderTest) {
 
     {
         // change a chunk and test again
-        state[0] ^= 0xFF; // Modify first byte to change first chunk
-        state[K12_chunkSize * 3] ^= 0xFF; // Modify a byte in the 3rd chunk
-        k12Engine.markChunkChanged(0);
-        k12Engine.markChunkChanged(3);
+        for (int i = 0; i < 1024 * 8; i++)
+        {
+            unsigned long long ran;
+            _rdrand64_step(&ran);
+            unsigned int chunkIndex = ran % k12Engine.getMaxChunks();
+            state[chunkIndex * K12_chunkSize] ^= 0xFF; // flip first byte of the chunk
+            k12Engine.markChunkChanged(chunkIndex);
+        }
         startTime = std::chrono::high_resolution_clock::now();
-        k12Engine.getHash128(hash1.m256i_u8, 32);
+        k12Engine.getHash(hash1.m256i_u8, 32);
         durationMilliSec = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startTime);
-        std::cout << "K12Engine getHash128 after modifying first chunk took " << durationMilliSec.count() << " ms" << std::endl;
+        std::cout << "K12Engine getHash after modifying chunks took " << durationMilliSec.count() << " ms" << std::endl;
         EXPECT_NE(hash1, hash2);
 
         // compare with XKCP again
